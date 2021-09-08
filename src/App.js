c6f8bcf69aea3c6a929839e60abc860c6ac1c7d3
roshnidhand <roshnidhand98@gmail.com>
2021-09-08 20:32:46 +0530

    first code1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/login";
+import Profile from "./pages/profile/profile";
+
+function App() {
+  return <Profile/>
+  
+}
+
+export default App;
